ae2795866cc0bda1dd92cdfffc0572ee20d000b7
Mikael RANDY <mikael.randy@gmail.com>
2012-01-21 17:23:54 +0100

    Mount drink controllers under "apero" base name

--- app/app.php
+++ app/app.php
@@ -2,6 +2,6 @@
 
 $app = require_once __DIR__.'/bootstrap.php';
 
-$app->mount('/', new Aperophp\Provider\Controller\Aperos());
+$app->mount('/apero', new Aperophp\Provider\Controller\Aperos());
 
 return $app;
