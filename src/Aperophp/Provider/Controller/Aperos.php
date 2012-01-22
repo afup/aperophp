167a984bcf419d08be6d24f7aa9d80b1ea95e0c7
Koin <pkoin.koin@gmail.com>
2012-01-22 21:13:54 +0100

    Création d'un controleur spécifique pour la gestion des membres.

--- src/Aperophp/Provider/Controller/Aperos.php
+++ src/Aperophp/Provider/Controller/Aperos.php
@@ -36,41 +36,6 @@ class Aperos implements ControllerProviderInterface
         });
         // *******
         
-        // *******
-        // ** Signup member
-        // *******
-        $controllers->get('member/signup.html', function() use ($app)
-        {
-            $form = $app['form.factory']->create(new \Aperophp\Form\Signup());
-            
-            return $app['twig']->render('member/signup.html.twig', array(
-                'form' => $form->createView(),
-            ));
-        })->bind('_signupmember');
-        // *******
-        
-        // *******
-        // ** Create member
-        // *******
-        $controllers->post('member/create.html', function(Request $request) use ($app)
-        {
-            $form = $app['form.factory']->create(new \Aperophp\Form\Signup());
-            
-            $form->bindRequest($request);
-            if ($form->isValid())
-            {
-                $data = $form->getData();
-                // TODO save member in database.
-                var_dump($data);
-                die;
-            }
-            
-            return $app['twig']->render('member/signup.html.twig', array(
-                'form' => $form->createView(),
-            ));
-        })->bind('_createmember');
-        // *******
-        
         return $controllers;
     }
 }
