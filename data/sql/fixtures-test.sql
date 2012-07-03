ea8479abc2f03c9b54e3b98d6831670516b5911f
Olivier Dolbeau <contact@odolbeau.fr>
2012-07-03 20:50:39 +0200

    Add more fixtures

--- data/sql/fixtures-test.sql
+++ data/sql/fixtures-test.sql
@@ -24,5 +24,21 @@ INSERT INTO `Member` (`id`, `username`, `password`) VALUES
 -- Contenu de la table `User`
 --
 
-INSERT INTO `User` (`lastname`, `firstname`, `email`, `member_id`) VALUES
-('Example', 'User', 'user@example.com', 1);
+INSERT INTO `User` (`id`, `lastname`, `firstname`, `email`, `member_id`) VALUES
+(1, 'Example1', 'User1', 'user1@example.com', null),
+(2, 'Example2', 'User2', 'user2@example.com', null),
+(3, 'Example3', 'User3', 'user3@example.com', 1);
+
+--
+-- Contenu de la table `Drink`
+--
+
+INSERT INTO `Drink` (`id`, `place`, `address`, `day`, `hour`, `kind`, `description`, `user_id`, `city_id`, `latitude`, `longitude`) VALUES
+(1, 'Au père tranquille', '16 rue Pierre Lescot, Paris, France', '2012-07-19', '19:30:00', 'drink', 'Apéro PHP de test au père tranquille', 3, 5, '48.86214', '2.34843');
+
+--
+-- Contenu de la table `Drink_Participation`
+--
+
+INSERT INTO `Drink_Participation` (`drink_id`, `user_id`, `percentage`, `reminder`) VALUES
+(1, 1, 75, 1);
