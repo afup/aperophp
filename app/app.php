bfc3ed865fe1fba4cd3a73ba8a18e84a1f4886b3
Mikael RANDY <mikael.randy@gmail.com>
2012-07-23 22:47:51 +0200

    Delete unuseful "drink" prefix on urls

--- app/app.php
+++ app/app.php
@@ -2,9 +2,9 @@
 
 $app = require_once __DIR__.'/bootstrap.php';
 
-$app->mount('/drink', new Aperophp\Provider\Controller\Drink());
-$app->mount('/drink/participation', new Aperophp\Provider\Controller\Participate());
-$app->mount('/drink/comment', new Aperophp\Provider\Controller\Comment());
+$app->mount('/', new Aperophp\Provider\Controller\Drink());
+$app->mount('/participation', new Aperophp\Provider\Controller\Participate());
+$app->mount('/comment', new Aperophp\Provider\Controller\Comment());
 $app->mount('/member', new Aperophp\Provider\Controller\Member());
 
 return $app;
