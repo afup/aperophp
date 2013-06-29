85b07ef0e4baeb58352c761837912a225ed88abb
Mikael Randy <mrandy.externe@m6.fr>
2013-06-29 23:08:11 +0200

    Add: Atoum configuration file

---
+++ .atoum.php
@@ -0,0 +1,31 @@
+<?php
+
+$runner->disableCodeCoverage();
+
+$script->addTestAllDirectory(__DIR__ . '/tests/units');
+
+## Notifier (growlnotify)
+$images = __DIR__ . '/vendor/atoum/atoum/resources/images/logo';
+
+$report = $script->AddDefaultReport();
+
+if(syslibExist('growlnotify') )
+{
+    $notifier = new \mageekguy\atoum\report\fields\runner\result\notifier\image\growl();
+    $notifier
+        ->setSuccessImage($images . DIRECTORY_SEPARATOR . 'success.png')
+        ->setFailureImage($images . DIRECTORY_SEPARATOR . 'failure.png')
+    ; 
+    $report->addField($notifier, array(atoum\runner::runStop));
+}
+
+/**
+ * Return true if library is available on system
+ * 
+ * @param  string $libName
+ * @return boolean
+ */
+function syslibExist($libName)
+{
+    return !is_null(shell_exec(sprintf('command -v %s 2>/dev/null', $libName)));
+}
